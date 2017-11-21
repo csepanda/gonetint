94b03eeacd54b4ae7d9ab1a76d64f9e05e00e947
Andrey Bova <andrewbova@yandex.ru>
2017-11-21 04:13:21 +0300

    Add controller for interface list getter
    Map "/interfaces" to specified controller

--- server/apiv0/controller.go
+++ server/apiv0/controller.go
@@ -1,6 +1,9 @@
 package apiv0
 
 import (
+    "fmt"
+    "net/http"
+    "encoding/json"
     "github.com/gorilla/mux"
 )
 
@@ -18,6 +21,31 @@ func (api *ApiController) GetVersion() string {
 
 func (api *ApiController) Init(router *mux.Router) error {
     api.root = router.PathPrefix("/" + VERSION).Subrouter()
+    api.root.Path("/interfaces").HandlerFunc(interfaceList)
 
     return nil
+}
+
+func interfaceList(w http.ResponseWriter, r *http.Request) {
+    list, err := getInterfacesNames()
+    if err != nil {
+        handleServiceError(err, w)
+        return
+    }
+
+
+    jsonStr, e := json.Marshal(list)
+    if e != nil {
+        handleServiceError(serverError(e.Error()), w)
+    } else {
+        w.Write(jsonStr)
+    }
+}
+
+func handleServiceError(e *serviceError, w http.ResponseWriter) {
+    fmt.Fprintf(w, "{\"error\":\"%s\"}", e.message)
+    switch e.errType {
+        case SERVER_SIDE_ERROR:    w.WriteHeader(500)
+        case CLIENT_REQUEST_ERROR: w.WriteHeader(404)
+    }
 }
